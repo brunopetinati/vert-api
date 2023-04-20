12dcd30e9a213a23099675060127eef25c79d811
Bruno <brunopetinati@hotmail.com>
2023-04-20 16:51:59 -0300

    adicionado função score aos projetos

--- projects/views.py
+++ projects/views.py
@@ -10,7 +10,6 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 
 from accounts.models import CustomUser
-import ipdb
 
 class ProjectListCreate(generics.ListCreateAPIView):
     queryset = Project.objects.all()
